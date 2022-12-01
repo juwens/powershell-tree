84ef8021fa2d3fea4b4f4b1b893aab7beb0800ac
Jens <jaehrig.jens@gmail.com>
2022-12-01 19:14:43 +0100

    fix paths with "[fobar].xml"

--- tree.ps1
+++ tree.ps1
@@ -22,7 +22,7 @@ function Out-Children {
 
     for ($i = 0; $i -lt $items.Count; $i++) {
         $item = $items[$i]
-        $name = (Get-Item $item).Name
+        $name = (Get-Item -LiteralPath $item).Name
         $prefix = $Prefix
         $isLastItem = $i -eq ($items.Count - 1)
 
@@ -52,7 +52,7 @@ Function Format-FileSize() {
     ElseIf ($size -gt 1GB) {[string]::Format("{0:0.0} GB", $size / 1GB)}
     ElseIf ($size -gt 1MB) {[string]::Format("{0:0.0} MB", $size / 1MB)}
     ElseIf ($size -gt 1KB) {[string]::Format("{0:0.0} kB", $size / 1KB)}
-    ElseIf ($size -ge 0)   {[string]::Format("{0:0.0} B", $size)}
+    ElseIf ($size -ge 0)   {[string]::Format("{0:0} B", $size)}
     Else                   {""}
 }
 
